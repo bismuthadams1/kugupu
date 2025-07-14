12c72ac655bfafbc9a1bc3442bf9a606470d52d8
Charlie Adams <k2584788@erc-hpc-mimic3.create.kcl.ac.uk>
2025-07-14 15:35:12 +0100

    gitignore exception

---
+++ make_env.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+##SBATCH -A DCCADD
+#SBATCH --mail-type=ALL
+#SBATCH --export=ALL
+#SBATCH -c 2
+#SBATCH --mem=4G   # Set total memory for the job
+#SBATCH --partition=cpu
+#SBATCH --cpus-per-task=1      
+
+mamba env create -f kugupu/forked_minimal.yml -y
